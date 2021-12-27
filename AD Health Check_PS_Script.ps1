3c03da0dd821f1b4422e8e77d88af321e43e9c00
MAYANK-GIT2020 <76818807+MAYANK-GIT2020@users.noreply.github.com>
2021-12-27 17:45:32 +0530

    Add files via upload
    #PS script upload. Author : Mayank Tripathi
    
    Developed highly automated PowerShell script binded with batch script, which was setup with Scheduler to trigger my PS script. The PS script collect all the server Health report in csv for multiple domain. 
    
    A csv file with detail report was sent along with Replication result. The script also produce the outfile for Trust (one way & two way) for inter forest AD environment. 
    
    The Health Check result was further used to published the data on internal dashboard for the presentation purpose for the server uptime and performance monitoring with Engineering Team.

---
+++ AD Health Check_PS_Script.ps1
@@ -0,0 +1,46 @@
+﻿#*****************AD HealthCheck and replication summary result****created by Mayank Tripathi(25/12/2021)
+repadmin /replsum | Out-File -FilePath C:\Users\Administrator\Documents\healthcheckscript\replication-summary.txt
+Out-File -FilePath C:\Users\Administrator\Documents\healthcheckscript\replication-summary.txt -Append -InputObject "---------------------C DRIVE STATUS---------------------" -Width 100
+$allinfo = @()
+$StartTime = (get-date).ToString('T')
+
+$getForest = [system.directoryservices.activedirectory.Forest]::GetCurrentForest()
+$DCServers = @($getForest.domains | ForEach-Object {$_.DomainControllers} | ForEach-Object {$_.Name} )
+$DCServers
+
+foreach ($DC in $DCServers){
+$DC | Out-File -FilePath C:\Users\Administrator\Documents\healthcheckscript\replication-summary.txt -Append
+$d = @(Invoke-Command -ComputerName $DC {Get-PSDrive | Where {$_.Free -gt 0}})| Out-File -FilePath C:\Users\Administrator\Documents\healthcheckscript\replication-summary.txt -Append
+}
+
+Out-File -FilePath C:\Users\Administrator\Documents\healthcheckscript\replication-summary.txt -Append -InputObject "---------------------Replication verification---------------------" -Width 100
+DCDIAG /test:CheckSecurityError | Out-File -FilePath C:\Users\Administrator\Documents\healthcheckscript\replication-summary.txt -Append
+
+$body = netdom trust nda1f1r1.com /Domain:nda1f2r1.com /twoway /Verify /verbose /UserO:Administrator /PasswordO:******* /UserD:Administrator /PasswordD:*******
+#$body
+
+$EndTime =(get-date).ToString('T')
+
+#SMTP code for mail alert 
+
+$fromaddress = "noreply@abcc.in"
+$toaddress = "xyz@abcc.abcc,xyz@abcc.abcc"
+$Subject = "PAS ENV1 Replication Summary Result"
+$bdy = "Hi,<br><br>"
+$bdy += "Please find the attached Replication summary result for PAS Environment 1.<br>"
+$bdy += "Trigger started at $StartTime and end time at: $EndTime.<br><br>"
+$bdy += "Regards,<br>"
+$bdy += "IT Team"
+$filename = "C:\Users\Administrator\Documents\healthcheckscript\replication-summary.txt"
+$attachment = "$filename"
+$smtpserver = "xxx.xxx.xx.xx"
+$message = new-object System.Net.Mail.MailMessage
+$message.From = $fromaddress
+$message.To.Add($toaddress)
+$message.IsBodyHtml = $True
+$message.Subject = $Subject
+$attach = new-object Net.Mail.Attachment($attachment)
+$message.Attachments.Add($attach)
+$message.body = $bdy
+$smtp = new-object Net.Mail.SmtpClient($smtpserver)
+$smtp.Send($message)
